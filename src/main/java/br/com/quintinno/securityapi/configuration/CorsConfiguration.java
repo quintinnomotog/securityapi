31a5196cf8c4b359a31461062d6e9e4e14b380c9
José Quintinno <josequintino@hotmail.com.br>
2025-06-10 10:39:34 -0300

    refactor: Implementar configuração de CORS

--- src/main/java/br/com/quintinno/securityapi/configuration/CorsConfiguration.java
+++ src/main/java/br/com/quintinno/securityapi/configuration/CorsConfiguration.java
@@ -1,5 +1,6 @@
 package br.com.quintinno.securityapi.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,10 +8,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfiguration implements WebMvcConfigurer {
 
+    @Value("${api.securityapi.url.origins}")
+    private String url;
+
     @Override
     public void addCorsMappings(CorsRegistry corsRegistry) {
          corsRegistry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")
+                .allowedOrigins(this.url)
                 .allowedMethods("GET", "POST", "DELETE", "PUT");
     }
 
