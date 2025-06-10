87dc2012bba448171ed9ab7d355861eb43f1398c
José Quintinno <josequintino@hotmail.com.br>
2025-06-09 21:33:58 -0300

    refactor: Alterar métodos de SigIn e SigUp

--- src/main/java/br/com/quintinno/securityapi/controller/UsuarioController.java
+++ src/main/java/br/com/quintinno/securityapi/controller/UsuarioController.java
@@ -1,18 +1,21 @@
 package br.com.quintinno.securityapi.controller;
 
+import java.util.HashMap;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 
 @RestController
 @RequestMapping("/usuario")
 public class UsuarioController {
 
     @GetMapping
-    public ResponseEntity<String> getUsuario() {
-        return ResponseEntity.ok().body("Requisição Realizada com Sucesso!");
+    public ResponseEntity<HashMap> getUsuario() {
+        HashMap hashMap = new HashMap<>();
+            hashMap.put("mensagem", "Requisição Realizada com Sucesso!");
+        return ResponseEntity.ok().body(hashMap);
     }
 
 }
